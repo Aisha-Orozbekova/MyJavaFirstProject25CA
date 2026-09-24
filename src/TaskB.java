0240470bf5e3b50bdc7e0ff78ea15f16c5cec53c
Aisha Orozbekova <aishyli4ka@gmail.com>
2026-09-24 12:10:04 +0600

    First commit

---
+++ src/TaskB.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class TaskB {
+    static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println("The next number for the number " + n + " is " + (n + 1)+".");
+        System.out.println("The previous number for the number " + n +" is " + (n - 1) +".");
+
+
+    }
+}
