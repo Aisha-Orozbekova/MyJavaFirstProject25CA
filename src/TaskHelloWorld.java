dd946fdb58aa9e08dca22be21264ba4ff73cef55
Aisha Orozbekova <aishyli4ka@gmail.com>
2026-09-24 11:47:31 +0600

    First commit

--- src/TaskHelloWorld.java
+++ src/TaskHelloWorld.java
@@ -1,9 +1,16 @@
-public class TaskHelloWorld {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
 
-        System.out.println("Hello world!");
+        double c = Math.sqrt(a * a + b * b);
 
+        System.out.println(c);
+
+        scanner.close();
     }
 }
-
